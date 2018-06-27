88f10e9126fac0c64238852a9cf07ded0efdbbe7
Adam Cohn <adamcohn88@gmail.com>
2018-06-27 12:40:22 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,49 @@
-import React from 'react'
+import React from 'react';
 
-import Filters from './Filters'
-import PetBrowser from './PetBrowser'
+import Filters from './Filters';
+import PetBrowser from './PetBrowser';
 
 class App extends React.Component {
   constructor() {
-    super()
-
+    super();
     this.state = {
       pets: [],
       filters: {
-        type: 'all'
-      }
-    }
+        type: 'all',
+      },
+    };
+    this.addPetsToState = this.addPetsToState.bind(this);
+    this.filterPets = this.filterPets.bind(this);
+    this.fetchPets = this.fetchPets.bind(this);
+    this.onAdoptPet = this.onAdoptPet.bind(this);
+  }
+
+  filterPets(query = 'all') {
+    const newFilter = { ...this.state.filters };
+    newFilter.type = query;
+    this.setState({ filters: newFilter });
+  }
+
+  fetchPets() {
+    let url;
+    this.state.filters.type === 'all'
+      ? (url = '/api/pets')
+      : (url = `/api/pets?type=${this.state.filters.type}`);
+    fetch(url)
+      .then(res => res.json())
+      .then(pets => this.addPetsToState(pets));
+  }
+
+  addPetsToState(newPets) {
+    this.setState({ pets: newPets });
+  }
+
+  onAdoptPet(id) {
+    const newPet = this.state.pets.find(pet => pet.id === id);
+    const newPetsList = this.state.pets.slice();
+    newPet.isAdopted = !newPet.isAdopted;
+    newPetsList[newPetsList.indexOf(newPet)] = newPet;
+    this.setState({ pets: newPetsList });
   }
 
   render() {
@@ -24,16 +55,16 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.filterPets} onFindPetsClick={this.fetchPets} />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet} />
             </div>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React from 'react';
 
 class Filters extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.props.onChangeType(event.target.value);
+  }
+
+  handleSubmit(event) {
+    this.props.onFindPetsClick();
+  }
+
   render() {
     return (
       <div className="ui form">
         <h3>Animal type</h3>
         <div className="field">
-          <select name="type" id="type">
+          <select name="type" id="type" onChange={this.handleChange}>
             <option value="all">All</option>
             <option value="cat">Cats</option>
             <option value="dog">Dogs</option>
@@ -15,11 +29,13 @@ class Filters extends React.Component {
         </div>
 
         <div className="field">
-          <button className="ui secondary button">Find pets</button>
+          <button className="ui secondary button" onClick={this.handleSubmit}>
+            Find pets
+          </button>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default Filters
+export default Filters;
